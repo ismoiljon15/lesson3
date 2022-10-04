ad914f5bd6ee5fff78d7aaf00bbce829bef636b2
ismoiljon15 <114994170+ismoiljon15@users.noreply.github.com>
2022-10-04 19:24:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,91 @@
+console.log("JS in started");
+
+let x = 10
+
+x += 7 // x = x + 7
+
+x -= 12 // x = x - 12
+
+x *=3  //  x = x * 3
+
+x /= 5  // x = x / 5
+
+
+
+
+
+
+let y = 7
+
+// y++ -->  increment
+y++ // y += 1 // y = y + 1
+
+y++
+
+
+// y-- --> decrement
+y--
+
+console.log(y);
+
+
+// ** --> daraja
+let z = 3 ** 4 // 3 * 3 * 3 * 3
+console.log(z);
+
+
+
+
+
+// DOM - Document Object Module
+
+const title = document.querySelector(".title")
+
+
+
+
+function heading(){
+  title.innerHTML = "lorem ipsum"
+  title.style.background = "blue"
+  title.style.color = "red"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(x);
+
